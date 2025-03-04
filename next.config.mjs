d189c0e8603e62884828a0d1ca134d0cd7b6a364
SRK700 <papana.210@gmail.com>
2025-03-04 17:39:32 +0700

    new seo

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,15 @@ const nextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, max-age=3600, must-revalidate',
+            value: 'public, max-age=3600, must-revalidate, s-maxage=3600',
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains',
           },
         ],
       },
